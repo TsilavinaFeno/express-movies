c859da57dd2a038f0476e70af417b139b3263e64
Tsila <tsilanmjr2@gmail.com>
2025-03-06 11:32:59 +0300

    feat : add static and dynamic route

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ app.get('/', function (req, res) {
     res.send('Hello <b>world 236</b>');
 })
 
+app.get('/movies', (req, res) => {
+    res.send('Bientôt des films ici');
+})
+
+app.get('/movies/add', (req, res) => {
+    res.send(`Bientôt un formulaire ici`);
+})
+
+app.get('/movies/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Film numéro ${id}`);
+})
+
 app.listen(port, function(){
     console.log(`listening in port ${port}`);
 })
